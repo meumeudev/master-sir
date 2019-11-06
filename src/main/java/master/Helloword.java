672fa7a7f898d68d7e11e5fd6ee21d68c97bb110
modoue <modou2015diop@gmail.com>
2019-11-06 15:29:03 +0000

    first commit

---
+++ src/main/java/master/Helloword.java
@@ -0,0 +1,14 @@
+package master;
+
+public class Helloword {
+	public String getMessage(String p)
+	{if(p==null)
+		
+		return "Hello word";
+	else return p;
+	}
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+}
